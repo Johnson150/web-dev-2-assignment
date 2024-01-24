2803911feb50a514e7a6dd0d335b91cc037742dc
Johnson Giang <johnson.giang21@gmail.com>
2024-01-23 18:53:36 -0700

    dsadsadsa

---
+++ Unitconversion.js
@@ -0,0 +1,11 @@
+const kmtomi = (kilometers) => kilometers * 0.621371;
+const mitokm = (miles) => miles / 0.621371;
+
+const lbstokg = (pounds) => pounds * 0.453592;
+const kgtolbs = (kilograms) => kilograms / 0.453592;
+
+const celsiustofahrenheit = (celsius) => (celsius * 9 / 5) + 32;
+const fahrenheittocelsius = (fahrenheit) => (fahrenheit * 5 / 9) - 32;
+
+
+

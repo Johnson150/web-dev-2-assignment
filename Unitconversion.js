36dee8d1dae9c37e1a707bf98dbde6d5b9ac1438
Johnson Giang <johnson.giang21@gmail.com>
2024-01-23 19:16:27 -0700

    dsasddsadas

--- Unitconversion.js
+++ Unitconversion.js
@@ -8,4 +8,22 @@ const celsiustofahrenheit = (celsius) => (celsius * 9 / 5) + 32;
 const fahrenheittocelsius = (fahrenheit) => (fahrenheit * 5 / 9) - 32;
 
 
+const unitconverter = (startingUnit, endingUnit, value)
+switch ('${startingUnit}-${endingUnit}') {
+    case 'lb-kg':
+        return lbstokg(value);
+    case 'kg-lb':
+        return kgtolbs(value);
+    case 'celsius-fahrenheit':
+        return celsiustofahrenheit(value);
+    case 'fahrenheit-celsius':
+        return fahrenheittocelsius(value);
+    case 'km-mi':
+        return kmtomi(value);
+    case 'mi-km':
+        return mitokm(value);
+    default:
+        return null;
 
+
+}
